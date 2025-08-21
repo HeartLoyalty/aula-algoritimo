040f80570569300c336ad005ad84be9c6dc3c9b9
Juan L. Ferreira <juanl.ferreira@outlook.com>
2025-08-21 17:37:52 +0000

    update

--- Lista-1/ex-2.js
+++ Lista-1/ex-2.js
@@ -1,4 +1,4 @@
 import input from 'readline-sync';
 let N1=input.questionInt("digite o Numero: ")
- let resp1=(N1-3)*2;
+let resp1=(N1-3)*2;
 console.log("o numero é:",resp1);
--- Lista-1/ex-8.js
+++ Lista-1/ex-8.js
@@ -5,5 +5,5 @@ let num3=input.questionInt("digite o num3:")
 let resp1= num1 +10
 let resp2= num2  -5
 let resp3= num3  +2
-console.log(":",resp1":",resp2":",resp3 );
+console.log("resposta",resp1,resp2,resp3);
 
