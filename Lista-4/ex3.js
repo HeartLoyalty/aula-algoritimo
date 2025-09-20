e01a5965212c46935fdbed6fefdbec3e3602a27d
Juan L. Ferreira <juanl.ferreira@outlook.com>
2025-09-19 23:55:00 -0300

    up

--- Lista-4/ex3.js
+++ Lista-4/ex3.js
@@ -2,10 +2,10 @@ import input from "readline-sync"
 let n1 = input.questionInt("Digite o numero:")
 
 if(n1>10){
-    console.log("o Numero maior que 10 é:", n1);
+    console.log("o numero é maior que 10.");
     
 }
 else{
-    console.log("o NUmero menor ou igual a 10 é", n1);
+    console.log("o numero é menor ou igual a 10.");
     
 }
---
+++ Lista-4/ex7.js
@@ -0,0 +1,9 @@
+import input from "readline-sync"
+
+let n1 = input.questionInt("digite o primeiro numero:")
+let n2 = input.questionInt("digite o segundo numero:")
+let n3 = input.questionInt("digite o terceiro numero:")
+
+if(n1<n2<n3)
+if(n1<n2<n3)
+    
